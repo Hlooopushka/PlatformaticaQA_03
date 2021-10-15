5c84c18e323eb35c2f58d4807206214bd60900a6
Vitaly <vzhka@post.cz>
2021-10-15 12:59:22 +0300

    Добавлен базовый класс для тестов

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,36 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    private static WebDriver driver;
+
+    @BeforeMethod
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void after() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), 10);
+    }
+}
