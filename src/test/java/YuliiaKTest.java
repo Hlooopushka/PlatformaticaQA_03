c6a5738c6e90ba9f33ac14659ff8ac850eb7204d
Yennefer1173 <92054459+Yennefer1173@users.noreply.github.com>
2021-10-15 16:51:07 +0300

    Yuliia test (#6)
    * Yuliia test
    
    * Yuliia test

---
+++ src/test/java/YuliiaKTest.java
@@ -0,0 +1,71 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class YuliiaKTest {
+
+    private WebDriver driver;
+    String url = "https://www.predskazanie.ru/";
+
+    @BeforeMethod
+    public void setUp() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String webPath = "c:\\Users\\1980v\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, webPath);
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void setDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void predskazTest() throws InterruptedException {
+
+        String expectedResult = "https://www.predskazanie.ru/goldfish/";
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        WebElement divinationOfaWish = driver.findElement
+                (By.xpath("//div[@id='menu']//a[@title='Гадание на желание']"));
+        divinationOfaWish.click();
+        WebElement divinationGoldFish = driver.findElement
+                (By.xpath("//p[@class='intro-image']//img[@alt='Гадание Золотая Рыбка']"));
+        divinationGoldFish.click();
+
+        String actualResult = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+    @Test
+    public void predskazTest2() throws InterruptedException {
+
+        String expectedResult = "https://www.predskazanie.ru/ekat.shtml";
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        WebElement divinationOfaLove = driver.findElement
+                (By.xpath("//div[@id='menu']//a[@title='Гадание на любовь и отношения']"));
+        divinationOfaLove.click();
+        WebElement queenKatya = driver.findElement
+                (By.xpath("//div[@class='content-layer']//img[@alt='Гадание императрицы Екатерины']"));
+        queenKatya.click();
+
+        String actualResult = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+}
