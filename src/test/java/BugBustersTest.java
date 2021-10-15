7c537117da1315bb61a898bd14f9270e1ec81fbf
Andrey Teterin <robinhood.ex@gmail.com>
2021-10-15 12:50:45 -0400

    add more tests

--- src/test/java/BugBustersTest.java
+++ src/test/java/BugBustersTest.java
@@ -1,8 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,12 +14,14 @@ public class BugBustersTest {
 
     private WebDriver driver;
     WebDriverWait wait;
+    JavascriptExecutor js;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
 
         driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -35,7 +34,7 @@ public class BugBustersTest {
     }
 
     @Test
-    public void testAndreyTeterin() {
+    public void testAndreyTeterinAddressCheck() {
 
         final String ADDRESS = "0x141a281889581C97cd7e6D4EC0A9B064B08bb239";
 
@@ -48,5 +47,31 @@ public class BugBustersTest {
         WebElement addressHeader = driver.findElement(By.className("address"));
         wait.until(ExpectedConditions.visibilityOf(addressHeader));
         Assert.assertEquals(addressHeader.getText(), ADDRESS);
+    }
+
+    @Test
+    public void testAndreyTeterinNews() {
+
+        driver.get("https://ethermine.org/");
+
+        WebElement twitterButton = driver.findElement(By.className("open-twitter"));
+        WebElement twitterWidget = driver.findElement(By.className("twitter-container"));
+
+        twitterButton.click();
+
+        Assert.assertFalse(twitterWidget.isDisplayed());
+    }
+
+    @Test
+    public void testAndreyTeterinDarkTheme() {
+
+        driver.get("https://ethermine.org/");
+
+        WebElement darkThemeButton = driver.findElement(By.className("theme-switch"));
+        WebElement html = driver.findElement(By.xpath("//html"));
+
+        js.executeScript("arguments[0].click();", darkThemeButton);
+
+        Assert.assertEquals(html.getAttribute("data-theme"), "dark");
     }
 }
