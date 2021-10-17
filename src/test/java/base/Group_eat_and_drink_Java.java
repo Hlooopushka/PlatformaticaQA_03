23649262c6544a181f2e7b20f67ca3b73d559450
TatianaTOROP <t.toropova1601@gmail.com>
2021-10-17 14:48:27 -0700

    Code cleaned

--- src/test/java/base/Group_eat_and_drink_Java.java
+++ src/test/java/base/Group_eat_and_drink_Java.java
@@ -130,61 +130,61 @@ public class Group_eat_and_drink_Java {
     }
 
     @Test
-        public void testRegistration() throws InterruptedException {
-            driver.get("https://humans.net/");
-            WebElement signUp = driver.findElement
-                    (By.xpath("//a[text()='Sign up']"));
-            signUp.click();
-            Thread.sleep(3000);
+    public void testRegistration() throws InterruptedException {
+        driver.get("https://humans.net/");
+        WebElement signUp = driver.findElement
+                (By.xpath("//a[text()='Sign up']"));
+        signUp.click();
+        Thread.sleep(3000);
 
-            WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
-            userName.sendKeys("8883468487");
-            WebElement password = driver.findElement(By.name("password"));
-            password.sendKeys("humans");
+        WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
+        userName.sendKeys("8883468487");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("humans");
 
-            WebElement joinNow = driver.findElement(By.id("reg-step-1"));
-            joinNow.click();
+        WebElement joinNow = driver.findElement(By.id("reg-step-1"));
+        joinNow.click();
 
-            WebElement codeBox1 = driver.findElement(By.name("digit0"));
-            codeBox1.sendKeys("1");
+        WebElement codeBox1 = driver.findElement(By.name("digit0"));
+        codeBox1.sendKeys("1");
 
-            WebElement codeBox2 = driver.findElement(By.name("digit1"));
-            codeBox2.sendKeys("2");
+        WebElement codeBox2 = driver.findElement(By.name("digit1"));
+        codeBox2.sendKeys("2");
 
-            WebElement codeBox3 = driver.findElement(By.name("digit2"));
-            codeBox3.sendKeys("3");
+        WebElement codeBox3 = driver.findElement(By.name("digit2"));
+        codeBox3.sendKeys("3");
 
-            WebElement codeBox4 = driver.findElement(By.name("digit3"));
-            codeBox4.sendKeys("4");
+        WebElement codeBox4 = driver.findElement(By.name("digit3"));
+        codeBox4.sendKeys("4");
 
-            WebElement continueButton = driver.findElement(By.xpath("//button[text()='Continue']"));
-            continueButton.click();
+        WebElement continueButton = driver.findElement(By.xpath("//button[text()='Continue']"));
+        continueButton.click();
 
-            WebElement error = driver.findElement(By.xpath("//div[text()='Incorrect verification code']"));
+        WebElement error = driver.findElement(By.xpath("//div[text()='Incorrect verification code']"));
 
-            Assert.assertEquals(error.getText(), "Incorrect verification code");
-        }
-
-        @Test
-        public void testLogInIncorrectValues() throws InterruptedException {
-            driver.get("https://humans.net/registration");
-            WebElement logIn = driver.findElement(By.xpath("//span[text()='Log in']"));
-            logIn.click();
-            Thread.sleep(3000);
-
-            WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
-            userName.sendKeys("8883468487");
-
-            WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
-            password.sendKeys("humans");
-
-            WebElement continueButton = driver.findElement(By.xpath("//button[@type='submit']/span"));
-            continueButton.click();
-
-            WebElement error = driver.findElement(By.xpath("//span[text()='Incorrect login or password']"));
-
-            Assert.assertEquals(error.getText(), "Incorrect login or password");
-
-        }
+        Assert.assertEquals(error.getText(), "Incorrect verification code");
     }
+
+    @Test
+    public void testLogInIncorrectValues() throws InterruptedException {
+        driver.get("https://humans.net/registration");
+        WebElement logIn = driver.findElement(By.xpath("//span[text()='Log in']"));
+        logIn.click();
+        Thread.sleep(3000);
+
+        WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
+        userName.sendKeys("8883468487");
+
+        WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
+        password.sendKeys("humans");
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[@type='submit']/span"));
+        continueButton.click();
+
+        WebElement error = driver.findElement(By.xpath("//span[text()='Incorrect login or password']"));
+
+        Assert.assertEquals(error.getText(), "Incorrect login or password");
+
+    }
+}
 
