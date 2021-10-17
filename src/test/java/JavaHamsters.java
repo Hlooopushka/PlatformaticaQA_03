758cadb464e07b01d2ceb36776492f752bba8e30
NadezdhaDekhand <nadeshka7979@gmail.com>
2021-10-17 19:13:56 +0700

    попытка №....

--- src/test/java/JavaHamsters.java
+++ src/test/java/JavaHamsters.java
@@ -13,6 +13,8 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.assertEquals;
+
 public class JavaHamsters {
 
     private final String URL_IK = "https://www.vprok.ru/";
@@ -192,5 +194,38 @@ public class JavaHamsters {
         String result = driver.getCurrentUrl();
 
         Assert.assertEquals(result,"https://www.prestashop.com/en");
+    }
+    @Test
+    public void NadezdhaDekhandLogo() {
+        driver.get("https://gb.ru/");
+
+        WebElement link = driver.findElement(By.className("mn-header__logo-link"));
+        link.click();
+        assertEquals(driver.getCurrentUrl(), "https://gb.ru/");
+    }
+
+    @Test
+    public void NadezdhaDekhandEnter() {
+        driver.get("https://gb.ru/");
+
+        WebElement userPlan = driver.findElement(By.xpath("//div[contains(@class,'mn-header__left')]//a[text()='Мероприятия']"));
+        userPlan.click();
+        WebElement privat = driver.findElement(By.linkText("Личные консультации"));
+        privat.click();
+        WebElement form = driver.findElement(By.linkText("Записаться"));
+        form.click();
+        WebElement name1User = driver.findElement(By.id("full_name-3"));
+        WebElement name2User = driver.findElement(By.id("full_name-4"));
+        WebElement emailUser = driver.findElement(By.id("email-2"));
+        WebElement phoneUser = driver.findElement(By.id("phone-2"));
+        WebElement login = driver.findElement(By.cssSelector("#wf-form-email-form > input.submit-button.w-button"));
+        name1User.sendKeys("gdhghd");
+        name2User.sendKeys("fgdrgtr");
+        emailUser.sendKeys("abc@gmail.com");
+        phoneUser.sendKeys("9999999999");
+
+
+        login.click();
+        assertEquals(driver.getCurrentUrl(), "https://gb.ru/a/a1?to=https://gb.ru/posts&success_claim=");
     }
 }
